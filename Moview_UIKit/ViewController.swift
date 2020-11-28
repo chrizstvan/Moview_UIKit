ff9fbb40a022c036595002e2fcde3101a84ed6a6
Christian Stevanus <chris.stev30@gmail.com>
2020-11-28 12:07:07 +0700

    Initial Commit

---
+++ Moview_UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moview_UIKit
+//
+//  Created by Chris Stev on 28/11/20.
+//  Copyright © 2020 ADI Consulting Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
